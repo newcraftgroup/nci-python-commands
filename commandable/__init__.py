00c839e8710d2939cebd86c3ab3c14866656b69b
Job Tiel Groenestege <job.tiel@newcraftgroup.com>
2018-03-01 11:10:31 +0100

    Add shorthand package for Command class

--- commandable/__init__.py
+++ commandable/__init__.py
@@ -12,4 +12,4 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-from .command_loader import CommandLoader
+from .command_loader import CommandLoader, Command
